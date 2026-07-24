1b25d451f183e781718f63fe842604a6eed88869
dayanacast <dayanacv80@gmail.com>
2026-07-23 22:35:10 -0600

    WIP integrante 4 manejo de errores

---
+++ Caso1/Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace Caso1.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Forbidden()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        public ActionResult ServerError()
+        {
+            return View();
+        }
+
+        public ActionResult General()
+        {
+            return View();
+        }
+    }
+}
